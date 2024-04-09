fda252804c43f943bc222f32cf7d675452e63f6c
Khionu Sybiern <dev@khionu.net>
2024-04-09 12:12:14 -0700

    feat: add libfiveStudio package
    This change attempts to create a package for libfive Studio, and try 
    using it in place of OpenSCAD

--- flake.nix
+++ flake.nix
@@ -13,6 +13,36 @@
       pkgs = import nixpkgs { inherit system; };
     });
   in {
+    packages = eachSystem ({ pkgs, system }: {
+      libfiveStudio = { lib, fetchFromGitHub, stdenv, boost, cmake, eigen, pkg-config,
+                        libpng, guile, python3, qt5 }:
+        stdenv.mkDerivation (finalAttrs: {
+          pname = "libfiveStudio";
+          version = "248c15c57abd2b1b9ea0e05d0a40f579d225f00f";
+          src = fetchFromGitHub {
+            owner = "libfive";
+            repo = "libfive";
+            rev = "${finalAttrs.version}";
+            hash = "sha256-WIw2n59sRIb77GEcVzq8TtTL69qlQLuX0/FqulLnI0E=";
+          };
+         
+          doCheck = false;
+          nativeBuildInputs = [
+            boost
+            cmake
+            eigen
+            pkg-config
+            qt5.wrapQtAppsHook
+          ];
+         
+          buildInputs = [
+            guile
+            python3
+            qt5.qtbase
+            libpng
+          ];
+        });
+    });
     devShells = eachSystem ({ pkgs, system }: {
       default = pkgs.mkShell {
         packages = with pkgs; [
